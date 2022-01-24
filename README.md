f3fc2f37410e8378b6922e759a26dc8781c40751
yaojunyid <1007227432@qq.com>
2022-01-24 16:52:17 +0800

    第一次

---
+++ README.md
@@ -0,0 +1,6 @@
+# 工程简介
+
+https://blog.csdn.net/admin522043032/article/details/115694905
+
+# 延伸阅读
+
---
+++ src/main/java/com/train/zookeeper/zookeeper/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.train.zookeeper.zookeeper.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@ConditionalOnClass(Docket.class)
+@EnableKnife4j
+public class SwaggerConfig {
+
+    private static final String VERSION = "1.0";
+
+    @Value("${springfox.swagger2.enabled}")
+    private Boolean swaggerEnabled;
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(swaggerEnabled)
+                .groupName("SwaggerDemo")
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 添加摘要信息
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("接口文档")
+                .contact(new Contact("Zookeeper","http://javadaily.cn","yao_junyi@qq.com"))
+                .description("Zookeeper接口文档")
+                .version(VERSION)
+                .build();
+    }
+
+}
